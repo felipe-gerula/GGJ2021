a4b5063371f1e5af0964822e3f6f168adf180691
felipe-gerula <felipe.gerula@gmail.com>
2021-01-30 19:14:22 -0300

    Move Enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,34 @@ public class Enemy : MonoBehaviour
 {
 
     public int health = 100;
-    public GameObject deathEffect;
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 5f;
+    public bool attack = false;
 
+    private void Start() {
+        rb = this.GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update() {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);    
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+  
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -24,4 +50,21 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
 
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+       PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+       if (playerMovement != null)
+       {
+           playerMovement.TakeDamage(1);
+       } 
+       if (other.gameObject.CompareTag("Player"))
+        {
+            transform.position = new Vector3(player.position.x,player.position.y - 4f,player.position.z);
+        }
+    }
+
+   public static Vector3 GetRandomDir()
+   {
+       return new Vector3(UnityEngine.Random.Range(-1f,1f),UnityEngine.Random.Range(-1f,1f)).normalized;
+   }
 }
